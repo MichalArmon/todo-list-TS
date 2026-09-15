f4805bca05bbd1b12c0088a8ff8d6eea422f747f
Michal Armon <armonmic22003@gmail.com>
2026-09-15 13:22:14 +0300

    ב

--- src/components/TodoTable.tsx
+++ src/components/TodoTable.tsx
@@ -1,4 +1,5 @@
 import {
+  Box,
   Container,
   Table,
   TableBody,
@@ -7,7 +8,8 @@ import {
   TableRow,
 } from "@mui/material";
 
-const mockTodos = [
+import { DataGrid, type GridColDef } from "@mui/x-data-grid";
+const rows = [
   {
     id: "cn283h323",
 
@@ -32,30 +34,55 @@ const mockTodos = [
     status: "Pending",
   },
 ];
+const columns: GridColDef[] = [
+  { field: "text", headerName: "Task", flex: 2 },
+  { field: "category", headerName: "Category", flex: 1 },
+  { field: "date", headerName: "Date", flex: 1 },
+  { field: "status", headerName: "Status", flex: 1 },
+];
 
 function TodoTable() {
   return (
     <Container>
-      <Table>
-        <TableHead>
-          <TableCell>Task</TableCell>
-          <TableCell>Category</TableCell>
-          <TableCell>Date</TableCell>
-          <TableCell>Status</TableCell>
-        </TableHead>
-        {mockTodos.map((task) => {
-          return (
-            <TableRow>
-              {" "}
-              <TableCell>{task.text}</TableCell>
-              <TableCell>{task.category}</TableCell>
-              <TableCell>{task.date.toLocaleDateString()}</TableCell>
-              <TableCell>{task.status}</TableCell>
-            </TableRow>
-          );
-        })}
-      </Table>
+      <Box sx={{ width: "100%" }}>
+        <DataGrid
+          rows={rows}
+          columns={columns}
+          sx={{ width: "100%", height: "100%" }}
+          initialState={{
+            pagination: {
+              paginationModel: {
+                pageSize: 5,
+              },
+            },
+          }}
+          pageSizeOptions={[5]}
+          checkboxSelection
+          disableRowSelectionOnClick
+        />
+      </Box>
     </Container>
+    // <Container>
+    //   <Table>
+    //     <TableHead>
+    //       <TableCell>Task</TableCell>
+    //       <TableCell>Category</TableCell>
+    //       <TableCell>Date</TableCell>
+    //       <TableCell>Status</TableCell>
+    //     </TableHead>
+    //     {mockTodos.map((task) => {
+    //       return (
+    //         <TableRow>
+    //           {" "}
+    //           <TableCell>{task.text}</TableCell>
+    //           <TableCell>{task.category}</TableCell>
+    //           <TableCell>{task.date.toLocaleDateString()}</TableCell>
+    //           <TableCell>{task.status}</TableCell>
+    //         </TableRow>
+    //       );
+    //     })}
+    //   </Table>
+    // </Container>
   );
 }
 
